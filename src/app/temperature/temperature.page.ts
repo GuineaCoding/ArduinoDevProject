e763b6b0666ae26d82089c803299915fcc6a1bd8
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-12 01:28:36 +0000

    V 0.8 Fixing chart bugs

--- src/app/temperature/temperature.page.ts
+++ src/app/temperature/temperature.page.ts
@@ -25,75 +25,63 @@ export class TemperaturePage implements OnInit {
   constructor(private sensorDataService: SensorDataService) { }
 
   ngOnInit() {
-    this.fetchTemperatureDataForTimeFrame('hours', 24);
+    this.fetchTemperatureDataForTimeFrame('hours', 24, 'day');
   }
 
   updateTimeFrame(timeFrame: string) {
-    let value;
+    let hours;
     switch (timeFrame) {
       case 'hour':
-        value = 1;
+        hours = 1;
         break;
       case 'day':
-        value = 24;
+        hours = 24;
         break;
       case 'week':
-        value = 24 * 7;
+        hours = 24 * 7;
         break;
       case 'month':
-        value = 24 * 30; 
+        hours = 24 * 30; 
         break;
       case 'year':
-        value = 24 * 365; 
+        hours = 24 * 365; 
         break;
-    }
-
-    // this.fetchTemperatureDataForTimeFrame(timeFrame, value);
+      default:
+        console.error('Invalid time frame specified');
+        return;
+    }     
+   
+    this.fetchTemperatureDataForTimeFrame('hours', hours, timeFrame);
   }
+  
 
-  fetchTemperatureDataForTimeFrame(timeFrame: string, value: number | { year: number, month?: number }) {
-    let observable$;
-
-    if (timeFrame === 'hours') {
-      observable$ = this.sensorDataService.getSensorDataForLastHours(value as number);
-    } else if (timeFrame === 'days') {
-      observable$ = this.sensorDataService.getSensorDataForLastDays(value as number);
-    } else if (timeFrame === 'month') {
-      observable$ = this.sensorDataService.getSensorDataForMonth((value as { year: number, month: number }).year, (value as { year: number, month: number }).month);
-    } else if (timeFrame === 'year') {
-      observable$ = this.sensorDataService.getSensorDataForYear((value as { year: number }).year);
-    } else {
-      console.error('Invalid time frame specified');
-      return;
-    }
-
+  fetchTemperatureDataForTimeFrame(timeUnit: string, value: number, selectedTimeFrame: string) {
+    const observable$ = this.sensorDataService.getSensorDataForLastHours(value);
     observable$.subscribe(data => {
-      console.log("Fetched data:", data); // Log for debugging
-      this.processTemperatureData(data);
+      this.processTemperatureData(data, selectedTimeFrame);
     }, error => {
       console.error('Error fetching temperature data:', error);
     });
   }
 
-  processTemperatureData(data: any[]) {
-    // Filter the data to remove entries with invalid or non-numeric temperatures
+  processTemperatureData(data: any[], selectedTimeFrame: string) {
     const validData = data.filter(d => !isNaN(+d.temperature));
-  
-    // filtered data to temperatures and labels
     const temperatures = validData.map(d => +d.temperature);
-    const labels = validData.map(d => new Date(d.timestamp * 1000).toLocaleTimeString());
   
-    if (temperatures.length > 0) {
-      this.currentTemperature = temperatures[temperatures.length - 1];
-      this.minTemperature = Math.min(...temperatures);
-      this.maxTemperature = Math.max(...temperatures);
+    let labels;
+    if (selectedTimeFrame === 'hour' || selectedTimeFrame === 'day') {
+      labels = validData.map(d => new Date(d.timestamp * 1000).toLocaleTimeString());
+    } else {
+      labels = validData.map(d => new Date(d.timestamp * 1000).toLocaleString());
     }
-    console.log("Filtered data:", validData);
-    console.log("Processed temperatures:", temperatures);
-    console.log("Labels for chart:", labels);
+  
+    this.currentTemperature = temperatures[temperatures.length - 1];
+    this.minTemperature = Math.min(...temperatures);
+    this.maxTemperature = Math.max(...temperatures);
   
     this.setupTemperatureChart(labels, temperatures);
   }
+  
 
   setupTemperatureChart(labels: string[], temperatures: number[]) {
   
@@ -152,4 +140,6 @@ export class TemperaturePage implements OnInit {
     console.log("Chart data object:", data);
     console.log("Chart options object:", options);
   }
+
+  
 }
