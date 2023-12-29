77534f3e17482a440859a5a3b2b86bab190062af
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-29 03:26:26 +0000

    V0.9 Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,2 @@
 // src/environments/environment.prod.ts
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyA9I4T4U0Glm8hTdKuNJ8DtpfCpiNlUPTs",
-    authDomain: "arduinoprojectmain.firebaseapp.com",
-    projectId: "arduinoprojectmain",
-    databaseURL: "https://arduinoprojectmain-default-rtdb.europe-west1.firebasedatabase.app",
-    storageBucket: "arduinoprojectmain.appspot.com",
-    messagingSenderId: "857212523227",
-    appId: "1:857212523227:web:1c455c77ed5793670932fe",
-    measurementId: "G-CJCLJ3Z3D3"
-  }
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyA9I4T4U0Glm8hTdKuNJ8DtpfCpiNlUPTs",
-    authDomain: "arduinoprojectmain.firebaseapp.com",
-    projectId: "arduinoprojectmain",
-    databaseURL: "https://arduinoprojectmain-default-rtdb.europe-west1.firebasedatabase.app",
-    storageBucket: "arduinoprojectmain.appspot.com",
-    messagingSenderId: "857212523227",
-    appId: "1:857212523227:web:1c455c77ed5793670932fe",
-    measurementId: "G-CJCLJ3Z3D3"
-  }
-};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
