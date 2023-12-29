c6ba617e8845e9b03daeb155a29cc841a6f26561
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-29 02:37:47 +0000

    V0.9 working on the last design touches, working with android studio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,8 +42,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    // { title: 'Profile', url: '/profile', icon: 'person' },
+  ];
+
   constructor() {}
 }
