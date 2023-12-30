281e80345a76b10ee1e3c1f230499ae3fe56be0f
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-30 20:11:57 +0000

    V0.9.9 Fix design and last bugs

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,17 +6,24 @@ import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 
+
+// Injectable decorator mraks the class as one that paritcipates in the dependency injection syste
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(private afAuth: AngularFireAuth,  private router: Router, private loadingController: LoadingController  ) {}
 
+  // Method for logging in a user with email and password
   login(email: string, password: string) {
+    // Returns a promise that resolves with user credentials after successful login
     return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
+  // Method to get the current authenticated user
   getUser() {
+    // authState is an observable of the authentication state
+    // The pipe and map operator are used to transform the emitted value
     return this.afAuth.authState.pipe(map(auth => auth));
   }
 
@@ -25,19 +32,25 @@ export class AuthService {
       message: 'Logging out...',
       spinner: 'circles'
     });
+    // Presenting the loading indicator
     await loading.present();
 
     try {
+      // Attempting to sign out the user
       await this.afAuth.signOut();
-      await loading.dismiss();
-      this.router.navigateByUrl('/login');
+      await loading.dismiss(); // Dismiss the loading indicator on success
+      this.router.navigateByUrl('/login');  // Navigate to the login page after successful logout
+      // Attempting to sign out the user
     } catch (error) {
+      // Catch block for handling errors during logout
       await loading.dismiss();
       console.error('Error during logout:', error);
       
     }
   }
+  // Method for initiating a password reset for a given email
   resetPassword(email: string) {
+     // Returns a promise that resolves when the password reset email is sent
     return this.afAuth.sendPasswordResetEmail(email);
   }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,18 +1,24 @@
+// Importing necessary modules
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs'; // Observable from RxJS library
 
+// Decorator that marks a class as available to be provided and injected as a dependency
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Specifies that the service should be provided in the root injector
 })
 export class FirebaseService {
+  // Constructor for injecting AngularFireDatabase service
   constructor(private db: AngularFireDatabase) {}
-
+  
   setHolidayModeStatus(isOn: boolean): Promise<void> {
+    // Sets the 'holidayMode/status' path in the Firebase database to the passed boolean value
     return this.db.object('holidayMode/status').set(isOn);
   }
 
   getHolidayModeState(): Observable<any> {
+    // Returns an observable for the value at 'holidayMode/status' in the Firebase database
+    // This observable emits the value whenever it changes in the database
     return this.db.object('holidayMode/status').valueChanges();
   }
 }
